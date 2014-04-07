127791c0c31de711a08058960d4bc2c69f4b0d04
SStupka <sstupka@slu.edu>
2014-04-06 21:01:53 -0500

    End of sprint updates. Dropdown menu and all problem types

---
+++ PhysicsSolver2/src/com/example/physicssolver2/ExpandableListAdapter.java
@@ -0,0 +1,105 @@
+package com.example.physicssolver2;
+import java.util.HashMap;
+import java.util.List;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private  Context _context;
+    private List<String> _listDataHeader;
+    private HashMap<String, List<String>> _listDataChild;
+
+    public ExpandableListAdapter(Context context, List<String> listDataHeader,
+                                 HashMap<String, List<String>> listDataChild)
+    {
+        this._context = context;
+        this._listDataChild = listDataChild;
+        this._listDataHeader = listDataHeader;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition)
+    {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPostion)
+    {
+        return childPostion;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition, boolean isLastChild,
+                             View convertView, ViewGroup parent)
+    {
+        final String childText = (String)getChild(groupPosition, childPosition);
+        if(convertView == null)
+        {
+            LayoutInflater infalInflater = (LayoutInflater)this._context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.child, null);
+        }
+
+        TextView textListChild = (TextView)convertView.findViewById(R.id.lblListItem);
+        textListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition)
+    {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition)
+    {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount()
+    {
+        return this._listDataHeader.size();
+    }
+    @Override
+    public long getGroupId(int groupPosition)
+    {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent)
+    {
+        String headerTitle = (String)getGroup(groupPosition);
+        if(convertView == null)
+        {
+            LayoutInflater infalInflater = (LayoutInflater)this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.group, null);
+        }
+
+        TextView lblListHeader = (TextView)convertView.findViewById(R.id.lblListHeader);
+        //lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition)
+    {
+        return true;
+    }
+}
