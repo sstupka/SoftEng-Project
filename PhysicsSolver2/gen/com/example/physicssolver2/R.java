10e1727c37bed4a82962c4182d71ce8427567b50
novellus <novellus.cato@gmail.com>
2014-04-15 21:06:45 -0500

    Updated old reference to _physicssolver2.

--- PhysicsSolver2/gen/com/example/physicssolver2/R.java
+++ PhysicsSolver2/gen/com/example/physicssolver2/R.java
@@ -8,6 +8,11 @@
 package com.example.physicssolver2;
 
 public final class R {
+    public static final class array {
+        public static final int PendulumSolveFor=0x7f060002;
+        public static final int meterUnits=0x7f060000;
+        public static final int timeUnits=0x7f060001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -28,34 +33,40 @@ public final class R {
         public static final int simplependulum=0x7f020004;
     }
     public static final class id {
-        public static final int EditText2=0x7f08000d;
-        public static final int EditText3=0x7f080012;
-        public static final int TextView01=0x7f080011;
-        public static final int action_settings=0x7f080013;
-        public static final int editText1=0x7f08000c;
-        public static final int horizontalScrollView1=0x7f080000;
-        public static final int imageView1=0x7f080005;
-        public static final int imageView2=0x7f080006;
-        public static final int listView1=0x7f080001;
-        public static final int pendulum=0x7f080002;
-        public static final int radio0=0x7f080008;
-        public static final int radio1=0x7f080009;
-        public static final int radio2=0x7f08000a;
-        public static final int radioGroup1=0x7f080007;
-        public static final int tab1=0x7f080003;
-        public static final int tab2=0x7f080004;
-        public static final int textView1=0x7f08000b;
-        public static final int textView2=0x7f08000e;
-        public static final int textView3=0x7f080010;
-        public static final int textView4=0x7f08000f;
+        public static final int EditText2=0x7f09000b;
+        public static final int EditText3=0x7f090011;
+        public static final int TextView01=0x7f09000a;
+        public static final int action_settings=0x7f090017;
+        public static final int editText1=0x7f090006;
+        public static final int imageView1=0x7f090001;
+        public static final int imageView2=0x7f090002;
+        public static final int lblListHeader=0x7f090016;
+        public static final int lblListItem=0x7f090015;
+        public static final int lvExp=0x7f090000;
+        public static final int spinner1=0x7f090007;
+        public static final int spinner2=0x7f090004;
+        public static final int spinner3=0x7f090009;
+        public static final int spinner4=0x7f09000c;
+        public static final int spinner5=0x7f09000e;
+        public static final int spinner6=0x7f090012;
+        public static final int spinner7=0x7f090014;
+        public static final int textView1=0x7f090003;
+        public static final int textView2=0x7f09000f;
+        public static final int textView3=0x7f090010;
+        public static final int textView4=0x7f090005;
+        public static final int textView5=0x7f090008;
+        public static final int textView6=0x7f09000d;
+        public static final int textView7=0x7f090013;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_pendulum=0x7f030001;
+        public static final int child=0x7f030002;
+        public static final int group=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int pendulum=0x7f070001;
+        public static final int main=0x7f080000;
+        public static final int pendulum=0x7f080001;
     }
     public static final class string {
         public static final int _=0x7f050016;
@@ -71,6 +82,7 @@ public final class R {
         public static final int g=0x7f050014;
         public static final int g_=0x7f050017;
         public static final int heading_3=0x7f050007;
+        public static final int hello=0x7f05001c;
         public static final int l=0x7f050013;
         public static final int l_=0x7f050019;
         public static final int pendulum=0x7f050008;
@@ -109,10 +121,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
