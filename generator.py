918c04d607e4e3c710e2f0798b3ba1b893c16ca2
novellus <novellus.cato@gmail.com>
2014-05-02 20:26:35 -0500

    Renamed xmlgenerator to generator. This script now generates the xml and java code for each independent activity. Bugs are known to be present, so do not overwrite the core project files with these generations yet. Still need to generate code to integrate activities into the main screen. I might add a database entry for the main screen entry.

--- generator.py
+++ generator.py
@@ -13,78 +13,79 @@ def unitsMath(varNum,starOrDiv,units,unitsNum): #RBF Needs more units
     if(units=="m"):
         return '''
 
-                if(units'''+str(unitsNum)+'''.equals("nm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-9);
-                if(units'''+str(unitsNum)+'''.equals("μm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-6);
-                if(units'''+str(unitsNum)+'''.equals("mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-3);
-                if(units'''+str(unitsNum)+'''.equals("cm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-2);
-                if(units'''+str(unitsNum)+'''.equals("dm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-1);
-                if(units'''+str(unitsNum)+'''.equals("m"))  ;
-                if(units'''+str(unitsNum)+'''.equals("km")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3);
-                if(units'''+str(unitsNum)+'''.equals("Mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,6);
-                if(units'''+str(unitsNum)+'''.equals("Gm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9);'''
+            if(units'''+str(unitsNum)+'''.equals("nm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-9);
+            if(units'''+str(unitsNum)+'''.equals("μm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-6);
+            if(units'''+str(unitsNum)+'''.equals("mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-3);
+            if(units'''+str(unitsNum)+'''.equals("cm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-2);
+            if(units'''+str(unitsNum)+'''.equals("dm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-1);
+            if(units'''+str(unitsNum)+'''.equals("m"))  ;
+            if(units'''+str(unitsNum)+'''.equals("km")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3);
+            if(units'''+str(unitsNum)+'''.equals("Mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,6);
+            if(units'''+str(unitsNum)+'''.equals("Gm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9);'''
     if(units=="m^2"):
         return '''
 
-                if(units'''+str(unitsNum)+'''.equals("nm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-9)*Math.pow(10,-9);
-                if(units'''+str(unitsNum)+'''.equals("μm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-6)*Math.pow(10,-6);
-                if(units'''+str(unitsNum)+'''.equals("mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-3)*Math.pow(10,-3);
-                if(units'''+str(unitsNum)+'''.equals("cm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-2)*Math.pow(10,-2);
-                if(units'''+str(unitsNum)+'''.equals("dm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-1)*Math.pow(10,-1);
-                if(units'''+str(unitsNum)+'''.equals("m"))  ;
-                if(units'''+str(unitsNum)+'''.equals("km")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3);
-                if(units'''+str(unitsNum)+'''.equals("Mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,6)*Math.pow(10,6);
-                if(units'''+str(unitsNum)+'''.equals("Gm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9);'''
+            if(units'''+str(unitsNum)+'''.equals("nm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-9)*Math.pow(10,-9);
+            if(units'''+str(unitsNum)+'''.equals("μm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-6)*Math.pow(10,-6);
+            if(units'''+str(unitsNum)+'''.equals("mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-3)*Math.pow(10,-3);
+            if(units'''+str(unitsNum)+'''.equals("cm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-2)*Math.pow(10,-2);
+            if(units'''+str(unitsNum)+'''.equals("dm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-1)*Math.pow(10,-1);
+            if(units'''+str(unitsNum)+'''.equals("m"))  ;
+            if(units'''+str(unitsNum)+'''.equals("km")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3);
+            if(units'''+str(unitsNum)+'''.equals("Mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,6)*Math.pow(10,6);
+            if(units'''+str(unitsNum)+'''.equals("Gm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9);'''
     if(units=="m^3"):
         return '''
 
-                if(units'''+str(unitsNum)+'''.equals("nm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-9)*Math.pow(10,-9)*Math.pow(10,-9);
-                if(units'''+str(unitsNum)+'''.equals("μm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-6)*Math.pow(10,-6)*Math.pow(10,-6);
-                if(units'''+str(unitsNum)+'''.equals("mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-3)*Math.pow(10,-3)*Math.pow(10,-3);
-                if(units'''+str(unitsNum)+'''.equals("cm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-2)*Math.pow(10,-2)*Math.pow(10,-2);
-                if(units'''+str(unitsNum)+'''.equals("dm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-1)*Math.pow(10,-1)*Math.pow(10,-1);
-                if(units'''+str(unitsNum)+'''.equals("m"))  ;
-                if(units'''+str(unitsNum)+'''.equals("km")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3)*Math.pow(10,3);
-                if(units'''+str(unitsNum)+'''.equals("Mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,6)*Math.pow(10,6)*Math.pow(10,6);
-                if(units'''+str(unitsNum)+'''.equals("Gm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9)*Math.pow(10,9);'''
+            if(units'''+str(unitsNum)+'''.equals("nm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-9)*Math.pow(10,-9)*Math.pow(10,-9);
+            if(units'''+str(unitsNum)+'''.equals("μm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-6)*Math.pow(10,-6)*Math.pow(10,-6);
+            if(units'''+str(unitsNum)+'''.equals("mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-3)*Math.pow(10,-3)*Math.pow(10,-3);
+            if(units'''+str(unitsNum)+'''.equals("cm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-2)*Math.pow(10,-2)*Math.pow(10,-2);
+            if(units'''+str(unitsNum)+'''.equals("dm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,-1)*Math.pow(10,-1)*Math.pow(10,-1);
+            if(units'''+str(unitsNum)+'''.equals("m"))  ;
+            if(units'''+str(unitsNum)+'''.equals("km")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3)*Math.pow(10,3);
+            if(units'''+str(unitsNum)+'''.equals("Mm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,6)*Math.pow(10,6)*Math.pow(10,6);
+            if(units'''+str(unitsNum)+'''.equals("Gm")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9)*Math.pow(10,9);'''
     if(units=="s"):
         return '''
                 
-                if(units'''+str(unitsNum)+'''.equals("ns")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9);
-                if(units'''+str(unitsNum)+'''.equals("ms")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3);
-                if(units'''+str(unitsNum)+'''.equals("s"))  ;
-                if(units'''+str(unitsNum)+'''.equals("min")) n'''+str(varNum)+starOrDiv+'''=60;
-                if(units'''+str(unitsNum)+'''.equals("hr")) n'''+str(varNum)+starOrDiv+'''=60*60;
-                if(units'''+str(unitsNum)+'''.equals("day")) n'''+str(varNum)+starOrDiv+'''=60*60*24;
-                if(units'''+str(unitsNum)+'''.equals("yr")) n'''+str(varNum)+starOrDiv+'''=60*60*24*365;'''
+            if(units'''+str(unitsNum)+'''.equals("ns")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9);
+            if(units'''+str(unitsNum)+'''.equals("ms")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3);
+            if(units'''+str(unitsNum)+'''.equals("s"))  ;
+            if(units'''+str(unitsNum)+'''.equals("min")) n'''+str(varNum)+starOrDiv+'''=1.0/(60);
+            if(units'''+str(unitsNum)+'''.equals("hr")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60);
+            if(units'''+str(unitsNum)+'''.equals("day")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*24);
+            if(units'''+str(unitsNum)+'''.equals("yr")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*24*365);'''
     if(units=="s^2"):
         return '''
                 
-                if(units'''+str(unitsNum)+'''.equals("ns")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9);
-                if(units'''+str(unitsNum)+'''.equals("ms")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3);
-                if(units'''+str(unitsNum)+'''.equals("s"))  ;
-                if(units'''+str(unitsNum)+'''.equals("min")) n'''+str(varNum)+starOrDiv+'''=60*60;
-                if(units'''+str(unitsNum)+'''.equals("hr")) n'''+str(varNum)+starOrDiv+'''=60*60*60*60;
-                if(units'''+str(unitsNum)+'''.equals("day")) n'''+str(varNum)+starOrDiv+'''=60*60*24*60*60*24;
-                if(units'''+str(unitsNum)+'''.equals("yr")) n'''+str(varNum)+starOrDiv+'''=60*60*24*365*60*60*24*365;'''
+            if(units'''+str(unitsNum)+'''.equals("ns")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9);
+            if(units'''+str(unitsNum)+'''.equals("ms")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3);
+            if(units'''+str(unitsNum)+'''.equals("s"))  ;
+            if(units'''+str(unitsNum)+'''.equals("min")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60);
+            if(units'''+str(unitsNum)+'''.equals("hr")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*60*60);
+            if(units'''+str(unitsNum)+'''.equals("day")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*24*60*60*24);
+            if(units'''+str(unitsNum)+'''.equals("yr")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*24*365*60*60*24*365);'''
     if(units=="s^3"):
         return '''
                 
-                if(units'''+str(unitsNum)+'''.equals("ns")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9)*Math.pow(10,9);
-                if(units'''+str(unitsNum)+'''.equals("ms")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3)*Math.pow(10,3);
-                if(units'''+str(unitsNum)+'''.equals("s"))  ;
-                if(units'''+str(unitsNum)+'''.equals("min")) n'''+str(varNum)+starOrDiv+'''=60*60*60;
-                if(units'''+str(unitsNum)+'''.equals("hr")) n'''+str(varNum)+starOrDiv+'''=60*60*60*60*60*60;
-                if(units'''+str(unitsNum)+'''.equals("day")) n'''+str(varNum)+starOrDiv+'''=60*60*24*60*60*24*60*60*24;
-                if(units'''+str(unitsNum)+'''.equals("yr")) n'''+str(varNum)+starOrDiv+'''=60*60*24*365*60*60*24*365*60*60*24*365;'''
+            if(units'''+str(unitsNum)+'''.equals("ns")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,9)*Math.pow(10,9)*Math.pow(10,9);
+            if(units'''+str(unitsNum)+'''.equals("ms")) n'''+str(varNum)+starOrDiv+'''=Math.pow(10,3)*Math.pow(10,3)*Math.pow(10,3);
+            if(units'''+str(unitsNum)+'''.equals("s"))  ;
+            if(units'''+str(unitsNum)+'''.equals("min")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*60);
+            if(units'''+str(unitsNum)+'''.equals("hr")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*60*60*60*60);
+            if(units'''+str(unitsNum)+'''.equals("day")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*24*60*60*24*60*60*24);
+            if(units'''+str(unitsNum)+'''.equals("yr")) n'''+str(varNum)+starOrDiv+'''=1.0/(60*60*24*365*60*60*24*365*60*60*24*365);'''
     return '' #Unrecognized units
 
 for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
     fields=activity.split("\n\n")
     
-    javaSave=['''''']*3 #Save code to be written in various places throughout the java file here.
+    javaSave=['''''']*3+[[[],[],[]]]+['''''']*0 #Save code to be written in various places throughout the java file here.
+    #element 3 is lists of EditText t#, and associated spinner units#. In database order.
 
     #Begin writing XML Code
-    outputXmlFile=open("res/layout/"+fields[2]+".xml",'w')
+    outputXmlFile=open("res/layout/"+fields[2].lower()+".xml",'w')
 
     #Header
     outputXmlFile.write('''<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
@@ -194,6 +195,9 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
         t'''+str(i)+'''.addTextChangedListener(updateAnswer);'''
         javaSave[2]+='''
         EditText t'''+str(i)+'''=(EditText)findViewById(R.id.editText'''+str(i)+''');'''
+        javaSave[3][0].append(i) #edit text and textview numbers are the same
+        javaSave[3][1].append([])
+        javaSave[3][2].append([])
 
         fraction=varFields[2].split("/")
         if len(fraction)==2: #Has a denominator
@@ -220,6 +224,8 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
                 javaSave[1]+='''
         Spinner units'''+str(i)+'''=(Spinner)findViewById(R.id.spinner'''+str(i)+''');
         units'''+str(i)+'''.setOnItemSelectedListener(updateUnits);'''
+                javaSave[3][1][-1].append(i)
+                javaSave[3][2][-1].append(unit)
                 
             unit=units[-1]
             i+=1
@@ -234,6 +240,8 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
             javaSave[1]+='''
         Spinner units'''+str(i)+'''=(Spinner)findViewById(R.id.spinner'''+str(i)+''');
         units'''+str(i)+'''.setOnItemSelectedListener(updateUnits);'''
+            javaSave[3][1][-1].append(i)
+            javaSave[3][2][-1].append(unit)
 
             i+=1 #write the "/" symbol
             outputXmlFile.write('''
@@ -270,6 +278,8 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
                 javaSave[1]+='''
         Spinner units'''+str(i)+'''=(Spinner)findViewById(R.id.spinner'''+str(i)+''');
         units'''+str(i)+'''.setOnItemSelectedListener(updateUnits);'''
+                javaSave[3][1][-1].append(i)
+                javaSave[3][2][-1].append(unit)
                 
             unit=units[-1]
             i+=1
@@ -284,6 +294,8 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
             javaSave[1]+='''
         Spinner units'''+str(i)+'''=(Spinner)findViewById(R.id.spinner'''+str(i)+''');
         units'''+str(i)+'''.setOnItemSelectedListener(updateUnits);'''
+            javaSave[3][1][-1].append(i)
+            javaSave[3][2][-1].append(unit)
 
         else: #No denominator
             units=fraction[0].split("*")
@@ -309,6 +321,8 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
                 javaSave[1]+='''
         Spinner units'''+str(i)+'''=(Spinner)findViewById(R.id.spinner'''+str(i)+''');
         units'''+str(i)+'''.setOnItemSelectedListener(updateUnits);'''
+                javaSave[3][1][-1].append(i)
+                javaSave[3][2][-1].append(unit)
                 
             unit=units[-1]
             i+=1
@@ -323,6 +337,8 @@ for activity in inputFile.read().split("\n\n\n\n"): #3 blank lines is 4 /n's
             javaSave[1]+='''
         Spinner units'''+str(i)+'''=(Spinner)findViewById(R.id.spinner'''+str(i)+''');
         units'''+str(i)+'''.setOnItemSelectedListener(updateUnits);'''
+            javaSave[3][1][-1].append(i)
+            javaSave[3][2][-1].append(unit)
         outputXmlFile.write('''
 
     </LinearLayout>''')
@@ -357,9 +373,9 @@ public class '''+fields[2]+''' extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         res = getResources();
-        setContentView(R.layout.activity_pendulum);
+        setContentView(R.layout.'''+fields[2].lower()+''');
 
-        Spinner solveFor=(Spinner)findViewById(R.id.spinner2);
+        Spinner solveFor=(Spinner)findViewById(R.id.spinner1);
         solveFor.setOnItemSelectedListener(updateSolveFor);
         ''')
 
@@ -377,9 +393,24 @@ public class '''+fields[2]+''' extends Activity {
     }
     
     TextWatcher updateAnswer=new TextWatcher(){
-        public void afterTextChanged(Editable s) {
-            updateAll();
+        private EditText e;
+        private String old="";
+
+        public void updateAnswer(EditText et) { 
+            e=et;
         }
+
+        public void afterTextChanged(Editable s) {
+            if(!e.getText().equals(old)) { //Prevents recursion here.
+                updateAll();
+            }
+            old=e.getText().toString();
+
+            /*e.removeTextChangedListener(this);
+            updateAll();
+            e.addTextChangedListener(this);*/
+        }
+
         public void beforeTextChanged(CharSequence s, int start, int count, int after){}
         public void onTextChanged(CharSequence s, int start, int before, int count){}
     };
@@ -394,6 +425,72 @@ public class '''+fields[2]+''' extends Activity {
     private void updateAll() {''')
 
     outputJavaFile.write(javaSave[2])
+    outputJavaFile.write('''
+        String solveFor=((Spinner)findViewById(R.id.spinner1)).getSelectedItem().toString();''')
+
+    for units in javaSave[3][1]:
+        for unit in units:
+            outputJavaFile.write('''
+        String units'''+str(unit)+'''=((Spinner)findViewById(R.id.spinner'''+str(unit)+''')).getSelectedItem().toString();''')
+
+    i=-1
+    for var in fields[1].split("\n"): #Solve for calculations for each variable
+        i+=1
+        varFields=var.split(";")
+
+        outputJavaFile.write('''
+        if(solveFor.equals("'''+varFields[1]+'''")''')
+        k=-1
+        for editTextNum in javaSave[3][0]:
+            k+=1
+            if k!=i:
+                outputJavaFile.write(''' && t'''+str(editTextNum)+'''.length()>0''')
+        outputJavaFile.write(''') {''')
+
+        k=-1
+        for editText in javaSave[3][0]: #Inputs
+            k+=1
+            if k!=i:
+                outputJavaFile.write('''
+
+            float n'''+str(k)+'''=Float.parseFloat(t'''+str(editText)+'''.getText().toString());''')
+                L=-1
+                outputJavaFile.close()
+                outputJavaFile=open("src/com/example/physicssolver2/"+fields[2]+".java",'ab') #Now writing a byte stream since this portion of the code writes unicode characters
+                for units in javaSave[3][1][k]:
+                    L+=1
+                    outputJavaFile.write(unitsMath(k,'*',javaSave[3][2][k][L],units).encode('utf8'))
+                outputJavaFile.close()
+                outputJavaFile=open("src/com/example/physicssolver2/"+fields[2]+".java",'a')
+
+        #Answer!
+        outputJavaFile.write('''
+
+            double n_='''+varFields[3]+''';''')
+        L=-1
+        for units in javaSave[3][1][i]:
+            L+=1
+            outputJavaFile.close()
+            outputJavaFile=open("src/com/example/physicssolver2/"+fields[2]+".java",'ab') #Now writing a byte stream since this portion of the code writes unicode characters
+            outputJavaFile.write(unitsMath('_','/',javaSave[3][2][i][L],units).encode('utf8'))
+            outputJavaFile.close()
+            outputJavaFile=open("src/com/example/physicssolver2/"+fields[2]+".java",'a')
+
+        outputJavaFile.write('''
+
+            t'''+str(javaSave[3][0][i])+'''.setText(String.format("%.2e",n_));
+        }''')
+    outputJavaFile.write('''
+    }
+    
+    AdapterView.OnItemSelectedListener updateSolveFor = new AdapterView.OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) { 
+            updateAll();
+        }
+        public void onNothingSelected(AdapterView<?> adapterView) {} 
+    };
+    Resources res;
+}''')
 
     outputJavaFile.close()
 
